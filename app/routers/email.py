cb599337bc6e7ea04e12920be3327e5b33d8744c
Constantin Ilie <intern.constantin.ilie@assist.ro>
2026-09-15 09:38:48 +0300

    fix: added final '/' to routes

--- app/routers/email.py
+++ app/routers/email.py
@@ -6,7 +6,7 @@ from app.services.email import send_email
 
 router = APIRouter(prefix="/email", tags=["email"])
 
-@router.post("/send", response_model= SendEmailResponse, status_code=status.HTTP_200_OK)
+@router.post("/send/", response_model= SendEmailResponse, status_code=status.HTTP_200_OK)
 async def send(request:SendEmailRequest):
     await send_email(request.to, request.subject, request.template,  request.variables)
     return SendEmailResponse()
